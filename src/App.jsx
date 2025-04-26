1edd189451399cb5112a30ead17b43b2ed2af991
JFDESIGN10000 <jfelixbm@gmail.com>
2025-04-26 02:19:46 -0500

    commitcero

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div style={{ fontFamily: 'Arial, sans-serif', textAlign: 'center', marginTop: '2rem' }}>
+      <h1>¡Hola, Bienbenido, soy José Félix! 🚀</h1>
+      <p>Mi app React ya está corriendo con Webpack y Babel, mi codigo fuente está protegido</p>
+    </div>
+  );
+};
+
+export default App;
